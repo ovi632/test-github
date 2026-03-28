e5f977051c7c198ced266e2c227209d982b0fe5d
Ishika Tasnim <itasnim793@gmail.com>
2026-03-28 21:06:04 +0600

    update

---
+++ src/main/java/com/example/test_github/TestGuthub.java
@@ -0,0 +1,22 @@
+package com.example.test_github;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class TestGuthub
+{
+    @javafx.fxml.FXML
+    private TextField textF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void buttonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SECButtonOA(ActionEvent actionEvent) {
+    }
+}
